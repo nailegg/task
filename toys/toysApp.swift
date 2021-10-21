dac4fe7642cedaa66b86510e578d504efa66620e
Oans <nailegg13@gmail.com>
2021-10-21 22:57:37 +0900

    Initial Commit

---
+++ toys/toysApp.swift
@@ -0,0 +1,17 @@
+//
+//  toysApp.swift
+//  toys
+//
+//  Created by 이상현 on 2021/10/21.
+//
+
+import SwiftUI
+
+@main
+struct toysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
